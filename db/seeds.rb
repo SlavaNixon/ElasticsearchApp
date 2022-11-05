84fe6a398ce37354f7a7f2ea15f6336e66a8d974
Vyacheslav Litvinov <v.litvinov@sftpro.ru>
2022-11-06 02:20:45 +0300

    db seeds refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts "Deleting all articles..."
 Article.delete_all
+client = Elasticsearch::Client.new()
+client.indices.delete index: 'articles' if client.indices.exists?(index: 'articles')
 
 puts "Creating articles..."
 50.times{ |i| Article.create(name: "Name#{i}", author: "Author#{i}", content: "Content#{i}") }
