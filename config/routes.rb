4b34be4103bc6f2b2e1be79471c7e0968af665fd
Vyacheslav Litvinov <v.litvinov@sftpro.ru>
2022-11-05 17:54:41 +0300

    Added search form and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    collection { get :search }
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
